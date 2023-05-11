41acccb6c7c3cbcedcc791bb895eda01b89f1158
Logan <logancable@ymail.com>
2023-05-11 13:06:52 -0700

    test

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,72 @@
 import React from "react";
 import { Navbar } from "./";
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Button,
+  Toolbar,
+  Container,
+} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AlarmIcon from "@mui/icons-material/Alarm";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import { PhotoCamera } from "@mui/icons-material";
 
 const Main = () => {
-    return(
-        <div id="main">
-            <Navbar />
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container max-width="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello everyone this is a photo album and I'm trying to make this
+              sentence as long as possible so we can see how it looks like.
+            </Typography>
+            <IconButton aria-label="delete">
+              <DeleteIcon />
+            </IconButton>
+            <IconButton aria-label="delete" disabled color="primary">
+              <DeleteIcon />
+            </IconButton>
+            <IconButton color="secondary" aria-label="add an alarm">
+              <AlarmIcon />
+            </IconButton>
+            <IconButton color="primary" aria-label="add to shopping cart">
+              <AddShoppingCartIcon />
+            </IconButton>
+          </Container>
         </div>
-    )
-}
+      </main>
+    </>
+  );
+};
 
-export default Main
+export default Main;
